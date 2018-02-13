50f2ace2e11455e4c74547571f2a5848237fbb14
bnajafi <b.najafi87@gmail.com>
2018-02-13 16:19:39 +0100

    updating the notebook

--- Python Files/step3_Exploratory_Data_Analysis.py
+++ Python Files/step3_Exploratory_Data_Analysis.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -19,3 +20,43 @@ df.dtypes
 
 # Then for the numeric columns we can use .corr to find the correlation 
 df.corr()
+
+#We should pay attention that in the results all of the string (object) columns were automatically removed.
+
+#we can also find the correlation matrix only for some parameters, having a look on the correaltion matrix, 
+#we can observe that engine-size, highway-L/100km , and horsepower are notably correlated with the price, let's have a look on the correlation of these variables with the price
+df[["engine-size","highway-L/100km", "horsepower","price"]].corr()
+
+
+#ScatterPlot: Visualising correlation between numeric variables¶
+
+#In order to visualise the correaltion between two numeric variables we can use scatter plots.
+
+
+
+#In order to visualise the correaltion between two numeric variables we can use scatter plots.
+plt.scatter(df["engine-size"],df["price"])
+plt.xlabel("engine size")
+plt.ylabel("price")
+plt.title("Price Vs. Engine-size")
+
+#An alternative appraoch is using seaborn's regplot which also adds plot a regression line
+sns.regplot(data=df,x="engine-size",y="price")
+
+#Let's crete a similar plot to see the correlation of price and city-mpg
+sns.regplot(data=df,x="city-mpg",y="price")
+
+
+#We can clearly observe a negative correlation, we can verify it by calculating the corresponding correlation
+
+df[["city-mpg","price"]].corr()
+#Finally,we can observe the correlation between peak-rpm, which are poorly correlated, the fact that it is also verified by calculating their correaltion coefficient.
+
+sns.regplot(data=df,x="peak-rpm",y="price")
+df[["peak-rpm","price"]].corr()
+
+
+## Finding the correlation between categorical variables  and numeric variables
+#categorical variables are the ones that describe a property of a data unitand can have the type "object" or "int64". A promising way to visualize categorical variables is by using boxplots.
+
+sns.boxplot(x="body-style", y="price", data=df)
